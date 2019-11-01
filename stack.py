a095ec3f37f838c0fe8c38100ec33f5e89ccbc39
aliahmadcse <aliahmadcse@gmail.com>
2019-11-01 22:55:20 +0500

    adds stack.py

---
+++ stack.py
@@ -0,0 +1,12 @@
+#create an empty stack
+stack = []
+
+#push items into the stack
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+#pop items out of the stack
+x=stack.pop()
+print(x)
+print(stack)
